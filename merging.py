e9af3014536f11685cab65ba4695189407444cde
SNEHAaa2406 <snehamulik167@gmail.com>
2026-08-24 23:18:52 +0530

    Two lists merged

---
+++ merging.py
@@ -0,0 +1,14 @@
+# Merge Two Lists
+# Take two lists and create a third list containing elements from both
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+list3 = []
+
+for i in list1:
+    list3.append(i)
+
+for i in list2:
+    list3.append(i)
+
+print(list3)
