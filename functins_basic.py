ba4366beb634cf046886cc4a644d9cfc0d125013
SNEHAaa2406 <snehamulik167@gmail.com>
2026-09-02 22:33:55 +0530

    problem added

---
+++ functins_basic.py
@@ -0,0 +1,20 @@
+# def greet():
+#     print("Hello")
+# greet()
+# def greet(name):
+#     print("Hello",name)
+# greet("sneha")
+
+# def hello():
+#     print("Welcome to python")
+# hello()
+
+# def my_name(name):
+#     print(name)
+# my_name("Sneha")
+
+def motivation():
+    print("Keep Learning Keep Growing")
+motivation()
+motivation()
+motivation() 
