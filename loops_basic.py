f83ed6e2d83d23923c7fc153577a5ec90cdaa7e8
SNEHAaa2406 <snehamulik167@gmail.com>
2026-09-26 10:45:31 +0530

    Loops basic revision

---
+++ loops_basic.py
@@ -0,0 +1,36 @@
+# Q1 — for
+# Print numbers from 1 to 10.
+# for i in range (1,11):
+#     print(i)
+#     i=i+1
+
+
+# Q2 — while
+# Print numbers from 1 to 5 using while.
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+# Print numbers from 1 to 10, but stop when the number becomes 6.
+# i=1
+# for i in range(1,7):
+#     if i==6:
+#         break
+#     else:
+#         print(i)
+
+# Q4 — continue
+# Print numbers from 1 to 10, but skip 5.
+# for i in range (1,11):
+#     if i==5:
+#         continue
+#     else:
+#         print(i)
+
+# Write a function called:
+def future_project():
+
+# Use pass inside it.
+    pass 
